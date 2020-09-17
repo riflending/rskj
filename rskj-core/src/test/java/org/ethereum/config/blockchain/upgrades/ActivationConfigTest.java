ae748cae11a53f7d995724daac405e524640d2bc
Federico Jinich <jinichfede@gmail.com>
2020-09-17 18:18:09 -0300

    blake2f for iris hardfork

--- rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
+++ rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
@@ -76,7 +76,7 @@ public class ActivationConfigTest {
             "    rskip171: iris300",
             "    rskip172: iris300",
             "    rskip174: iris300",
-            "    rskip153: wasabiPlusOne",
+            "    rskip153: iris300",
             "}"
     ));
 
