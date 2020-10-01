ee9456f0f83cafe1d376c4e0981ad6140c570755
Federico Jinich <jinichfede@gmail.com>
2020-10-01 13:37:07 -0300

    blake2f testing without Pair class

--- rskj-core/src/test/java/org/ethereum/vm/PrecompiledContractTest.java
+++ rskj-core/src/test/java/org/ethereum/vm/PrecompiledContractTest.java
@@ -20,7 +20,6 @@
 package org.ethereum.vm;
 
 import co.rsk.config.TestSystemProperties;
-import javafx.util.Pair;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.config.blockchain.upgrades.ConsensusRule;
@@ -312,21 +311,22 @@ public class PrecompiledContractTest {
     }
 
     private void internalBlake2fTestVector(String data, String expectedResult, long expectedGasCost) {
-        Pair<byte[], Long> execution = callBlake2(data);
+        PrecompiledContract contract = blake2();
+        byte[] result = contract.execute(Hex.decode(data));
 
-        assertEquals(expectedResult, Hex.toHexString(execution.getKey()));
-        assertEquals(expectedGasCost, execution.getValue().longValue());
+        assertEquals(expectedResult, Hex.toHexString(result));
+        assertEquals(expectedGasCost, contract.getGasForData(Hex.decode(data)));
     }
 
     private void internalFailBlake2fTestVector(String data, String expectedError) {
         try {
-            callBlake2(data);
+            blake2().execute(Hex.decode(data));
         } catch (IllegalArgumentException e) {
             assertEquals(expectedError, e.getMessage());
         }
     }
 
-    private Pair<byte[], Long> callBlake2(String data) {
+    private PrecompiledContract blake2() {
         ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
         when(activations.isActive(ConsensusRule.RSKIP176)).thenReturn(true);
 
@@ -334,12 +334,7 @@ public class PrecompiledContractTest {
         PrecompiledContract contract = precompiledContracts.getContractForAddress(activations, addr);
         assertNotNull(contract);
 
-        byte[] dataBytes = Hex.decode(data);
-
-        byte[] result = contract.execute(dataBytes);
-        long gasCost = contract.getGasForData(dataBytes);
-
-        return new Pair(result, gasCost);
+        return contract;
     }
 
     @Test
